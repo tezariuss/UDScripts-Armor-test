1299256558effbba9377a56604d9bd8c09c3b8fb
Gamerooni <gamerinogamerooni@gmail.com>
2023-11-21 22:21:02 +1000

    Propertly implement settings into rest of script

--- UDPatcher/UDPatchSettings.cs
+++ UDPatcher/UDPatchSettings.cs
@@ -1,4 +1,4 @@
-using Mutagen.Bethesda.Fallout4;
+using Mutagen.Bethesda.Skyrim;
 using Mutagen.Bethesda.Plugins;
 using System;
 using System.Collections.Generic;
@@ -13,13 +13,39 @@ namespace UDPatcher
         public bool UseModes = false;
         public ModKey OutputPatchMod = ModKey.Null;
         public HashSet<ModKey> ModsToPatch = new();
-        public List<UDKwSettings> KeywordMatches = new();
+        public Dictionary<string, HashSet<string>> ScriptMatches = new();
+        public List<UDOtherSettings> OtherMatches = new();
+        //public 
+        //public UDKwSettings test = new();
+        //public IFormLinkGetter<IKeywordGetter> UDKeyword = FormLink<IKeywordGetter>.Null;
+        //public FormLink<IArmorGetter> testArmor = new();
         //public ModKey ModToPatch = ModKey.Null;
+    }
+
+    public class UDOtherSettings
+    {
+        /*public FormLink<IKeywordGetter> UDKeyword = new();
+        public HashSet<FormLink<IKeywordGetter>> KeywordsToMatch = new();*/
+        public HashSet<string> InputScripts = new HashSet<string>();
+        public UDKwSettings KeywordMatch = new();
+        public List<UDNameSearchSettings> NameMatch = new();
     }
 
     public class UDKwSettings
     {
-        public IFormLinkGetter<IKeywordGetter> UDKeyword = FormLink<IKeywordGetter>.Null;
-        public HashSet<IFormLinkGetter<IKeywordGetter>> KeywordsToMatch = new();
+        public HashSet<IFormLinkGetter<IKeywordGetter>> Keywords = new();
+        public string OutputScript = string.Empty;
+    }
+/*
+    public class UDNameSettings
+    {
+        public HashSet<string> InputScripts = new HashSet<string>();
+        public List<UDNameSearchSettings> SearchResults = new();
+    }*/
+
+    public class UDNameSearchSettings
+    {
+        public string SearchText = string.Empty;
+        public string OutputScript = string.Empty;
     }
 }
